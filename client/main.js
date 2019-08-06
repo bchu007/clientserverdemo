f0c10d90a2921cbbac6d8085da5a074099bdc0d2
Brian Chu <brianchu14@gmail.com>
2019-08-06 15:19:16 -0700

    store, read, and displayed data added data to server sent data through endpoint changed client address to correct server address put names on DOM

--- client/main.js
+++ client/main.js
@@ -2,21 +2,33 @@ $(document).ready( startApp );
 
 function startApp() {
 	getData();
+	getFood();
 }
 
 function getData() {
 	var settings = {
-		url: '../server/getnames.json',
+		url: 'http://localhost:3001/names',
 		method: 'get', 
 		dataType: 'json',
-		data: {
-			name: 'Joe'
-		},
+		success: handleDataFromServer
+	}
+	$.ajax(settings);
+}
+
+function getFood() {
+	var settings = {
+		url: 'http://localhost:3001/favfoods',
+		method: 'GET', 
+		dataType: 'json',
 		success: handleDataFromServer
 	}
 	$.ajax(settings);
 }
 
 function handleDataFromServer( response ) {
-	console.log(response);
+	for (var i = 0; i < response.length; i++) {
+		var nameDiv = $('<div>').text(response[i].name)
+		console.log(response[i])
+		$('body').append(nameDiv)
+	}
 }
